6341805431a0721d7f8a5ddfc50a515f8c452a67
mjmauldin <mjmauldin324@gmail.com>
2018-06-08 22:57:36 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -25,6 +25,43 @@ class LogSkill(MycroftSkill):
         # Initialize working variables used within the skill.
         self.count = 0
 
+        
+        
+    def text2int(textnum, numwords={}):
+        if not numwords:
+          units = [
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+            "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+            "sixteen", "seventeen", "eighteen", "nineteen",
+          ]
+
+          tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+          scales = ["hundred", "thousand", "million", "billion", "trillion"]
+
+          numwords["and"] = (1, 0)
+          for idx, word in enumerate(units):    numwords[word] = (1, idx)
+          for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
+          for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
+
+        current = result = 0
+        for word in textnum.split():
+            if word not in numwords:
+              raise Exception("Illegal word: " + word)
+
+            scale, increment = numwords[word]
+            current = current * scale + increment
+            if scale > 100:
+                result += current
+                current = 0
+
+        return result + current        
+        
+        
+        
+        
+        
+        
     # The "handle_xxxx_intent" function is triggered by Mycroft when the
     # skill's intent is matched.  The intent is defined by the IntentBuilder()
     # pieces, and is triggered when the user's utterance matches the pattern
@@ -67,35 +104,7 @@ class LogSkill(MycroftSkill):
     # def stop(self):
     #    return False
     
-    def text2int(textnum, numwords={}):
-        if not numwords:
-          units = [
-            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
-            "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-            "sixteen", "seventeen", "eighteen", "nineteen",
-          ]
 
-          tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-
-          scales = ["hundred", "thousand", "million", "billion", "trillion"]
-
-          numwords["and"] = (1, 0)
-          for idx, word in enumerate(units):    numwords[word] = (1, idx)
-          for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
-          for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
-
-        current = result = 0
-        for word in textnum.split():
-            if word not in numwords:
-              raise Exception("Illegal word: " + word)
-
-            scale, increment = numwords[word]
-            current = current * scale + increment
-            if scale > 100:
-                result += current
-                current = 0
-
-        return result + current
     
     
 
